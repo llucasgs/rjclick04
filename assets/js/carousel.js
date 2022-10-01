9ec8155dca97ecabfaadb0c0591d70761b95794a
Mateus Queirós <mateuscqueiros1@gmail.com>
2022-09-30 22:34:15 -0300

    JS e perfect vacations

---
+++ assets/js/carousel.js
@@ -0,0 +1,50 @@
+var currentPlace = 1;
+
+const links = document.querySelectorAll(".hero .link-item");
+const heroSection = document.querySelector(".hero");
+
+const places = [
+    {
+        name: "Cristo Redentor",
+        description: "Oitava maravilha do mundo",
+        image: "/assets/images/popular-places/cristo.jpg"
+    },
+    {
+        name: "Maracanã",
+        description: "O maior estádio do Brasil",
+        image: "/assets/images/popular-places/maracana.png"
+    },
+    {
+        name: "Jardim Botânico",
+        description: "O mais antigo Jardim Botânico da América Latina",
+        image: "/assets/images/popular-places/jardim_botanico.png"
+    },
+    {
+        name: "Pão de Açúcar",
+        description: "O ícone do Rio",
+        image: "/assets/images/popular-places/pao_de_acucar.png"
+    }
+];
+
+function showPlace() {
+
+    if (currentPlace > 3) currentPlace = 0;
+
+    const place = places[currentPlace];
+
+    heroSection.style.backgroundImage = `url(${place.image})`;
+
+    heroSection.querySelector(".title h1").innerText = place.name;
+    heroSection.querySelector(".title h2").innerText = place.description;
+
+    links.forEach(link => {
+        link.classList.remove('active');
+    });
+
+    heroSection.querySelector(`.link-item[data-place="${currentPlace}"]`).classList.add("active");
+
+    currentPlace++
+
+}
+
+let myInterval = setInterval(showPlace, 4000);
--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,3 +1,19 @@
 var linkMobileMenu = document.querySelector(".link-mobile-menu");
+var linkCloseMenu = document.querySelector(".close-menu");
+var overlay = document.querySelector(".overlay");
 
-linkMobileMenu.addEventListener("click");
+function openMenu(event) {
+    event.preventDefault();
+
+    document.body.classList.add("open");
+}
+
+function closeMenu(event) {
+    event.preventDefault();
+
+    document.body.classList.remove("open");
+}
+
+linkMobileMenu.addEventListener("click", openMenu);
+linkCloseMenu.addEventListener("click", closeMenu);
+overlay.addEventListener("click", closeMenu);
